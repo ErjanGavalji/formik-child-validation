54355ecf23d9c5403a10f6b67ba2f1e5a0edb201
Erjan Gavalji <erjan.gavalji@gmail.com>
2022-02-28 14:53:39 +0200

    Add react-bootsrap. Declare formik, its type and initialize it. Declare the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,56 @@
+import { useFormik } from "formik";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import logo from './logo.svg';
 import './App.css';
 
+interface Fruit {
+  name: string
+  color: string
+}
+
+interface Basket {
+  contents: Fruit[],
+  weight: number,
+}
+
+interface Cart {
+  singleFruits: Fruit[],
+  baskets?: Basket[],
+}
+
 function App() {
+  const initialCart: Cart = {
+    singleFruits: [
+      {
+        "name": "Pineapple",
+        "color": "green-yellow",
+      },
+      {
+        "name": "Watermelon",
+        "color": "green",
+      }
+    ]
+  }
+
+  function handleFormSubmit(values: Cart, errors: any)  {
+  }
+
+  const formik = useFormik<Cart>({
+    initialValues: initialCart,
+    onSubmit: handleFormSubmit,
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <a href="https://www.anjack.io"><img src={logo} className="App-logo" alt="Anjack Logo" /></a>
       </header>
+      <div>
+        <Form noValidate>
+
+        </Form>
+      </div>
     </div>
   );
 }
